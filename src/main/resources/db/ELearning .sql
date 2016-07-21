894f9a1cce35c6185ff481e61a912412bc5491bf
Andrei Iulian Juravle <andrei.juravle@info.uaic.ro>
2016-07-21 17:13:56 +0300

    add DataBase in e-learning\src\main\resources\db

--- src/main/resources/db/ELearning .sql
+++ src/main/resources/db/ELearning .sql
@@ -1,30 +1,15 @@
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2016-07-20 14:37:56.342
+DROP TABLE Keywords;
+DROP TABLE Materials;
+DROP TABLE Topics;
+DROP TABLE Technologies;
+DROP TABLE Users;
 
--- tables
--- Table: Keywords
-CREATE TABLE Keywords (
-    topic_id int NOT NULL,
-    keyword varchar(20) NOT NULL,
-    CONSTRAINT Keywords_pk PRIMARY KEY (topic_id,keyword)
-);
-
--- Table: Materials
-CREATE TABLE Materials (
-    material_id int NOT NULL,
-    link varchar(50) NOT NULL,
-    topic_id int NOT NULL,
-    CONSTRAINT Materials_pk PRIMARY KEY (material_id)
-);
-
--- Table: Technologies
 CREATE TABLE Technologies (
     technology_id int NOT NULL,
     name varchar(30) NOT NULL,
     CONSTRAINT Technologies_pk PRIMARY KEY (technology_id)
 );
 
--- Table: Topics
 CREATE TABLE Topics (
     topic_id int NOT NULL,
     name varchar(30) NOT NULL,
@@ -32,7 +17,19 @@ CREATE TABLE Topics (
     CONSTRAINT Topics_pk PRIMARY KEY (topic_id)
 );
 
--- Table: Users
+CREATE TABLE Materials (
+    material_id int NOT NULL,
+    link varchar(50) NOT NULL,
+    topic_id int NOT NULL,
+    CONSTRAINT Materials_pk PRIMARY KEY (material_id)
+);
+
+CREATE TABLE Keywords (
+    topic_id int NOT NULL,
+    keyword varchar(20) NOT NULL,
+    CONSTRAINT Keywords_pk PRIMARY KEY (topic_id,keyword)
+);
+
 CREATE TABLE Users (
     user_id int NOT NULL,
     user_type varchar(20) NOT NULL,
@@ -47,18 +44,16 @@ CREATE TABLE Users (
     CONSTRAINT Users_pk PRIMARY KEY (user_id)
 );
 
--- foreign keys
--- Reference: Materials_Topic (table: Materials)
-ALTER TABLE Materials ADD CONSTRAINT Materials_Topic FOREIGN KEY Materials_Topic (topic_id)
-    REFERENCES Topics (topic_id);
 
--- Reference: Table_10_Topic (table: Keywords)
-ALTER TABLE Keywords ADD CONSTRAINT Table_10_Topic FOREIGN KEY Table_10_Topic (topic_id)
-    REFERENCES Topics (topic_id);
+ALTER TABLE Materials ADD CONSTRAINT Materials_Topic FOREIGN KEY (topic_id)
+    REFERENCES Topics (topic_id) ON DELETE CASCADE;
 
--- Reference: Topic_Technologies (table: Topics)
-ALTER TABLE Topics ADD CONSTRAINT Topic_Technologies FOREIGN KEY Topic_Technologies (technology_id)
-    REFERENCES Technologies (technology_id);
 
--- End of file.
+ALTER TABLE Keywords ADD CONSTRAINT Table_10_Topic FOREIGN KEY (topic_id)
+    REFERENCES Topics (topic_id) ON DELETE CASCADE;
+
+
+ALTER TABLE Topics ADD CONSTRAINT Topic_Technologies FOREIGN KEY (technology_id)
+    REFERENCES Technologies (technology_id) ON DELETE CASCADE;
+
 
