01a2808004c1975803a8cc30a17bcfd6fe5a8608
Madalina Neghina <Madalina.Neghina@endava.com>
2016-07-22 09:43:42 +0300

    deleted some wrong files

--- e-learning/src/main/java/com/endava/learning/dao/KeywordDAO.java
+++
@@ -1,9 +0,0 @@
-package com.endava.learning.dao;
-
-import com.endava.learning.model.Keyword;
-
-public class KeywordDAO extends AbstractDAO{
-	protected KeywordDAO() {
-		super(Keyword.class);
-	}
-}
