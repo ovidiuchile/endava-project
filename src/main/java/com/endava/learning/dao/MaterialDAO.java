c8080c6cf05cc74cd1eaf11e58184cf70e5f9290
Andrei Iulian Juravle <andrei.juravle@info.uaic.ro>
2016-07-22 10:46:25 +0300

    add DAO files

---
+++ src/main/java/com/endava/learning/dao/MaterialDAO.java
@@ -0,0 +1,11 @@
+package com.endava.learning.dao;
+
+import com.endava.learning.model.Material;
+
+@SuppressWarnings("rawtypes")
+public class MaterialDAO extends AbstractDAO {
+	@SuppressWarnings("unchecked")
+	protected MaterialDAO() {
+		super(Material.class);
+	}
+}
