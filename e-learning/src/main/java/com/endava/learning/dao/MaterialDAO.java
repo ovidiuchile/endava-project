1fb4e0501bc7aca59255bb728ef04623eda6c92c
Madalina Neghina <Madalina.Neghina@endava.com>
2016-07-22 09:47:43 +0300

    deleted wrong files

--- e-learning/src/main/java/com/endava/learning/dao/MaterialDAO.java
+++
@@ -1,9 +0,0 @@
-package com.endava.learning.dao;
-
-import com.endava.learning.model.Material;
-
-public class MaterialDAO extends AbstractDAO{
-	protected MaterialDAO() {
-		super(Material.class);
-	}
-}
--- e-learning/src/main/java/com/endava/learning/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.endava.learning.dao;
-
-import com.endava.learning.model.User;
-
-public class UserDAO extends AbstractDAO{
-	protected UserDAO() {
-		super(User.class);
-	}
-}
