07cf1affe05c419bc8d333bfb7cea2653cdd5664
Andrei Iulian Juravle <andrei.juravle@info.uaic.ro>
2016-07-25 09:38:29 +0300

    sadfhagvfhdb

--- src/main/java/com/endava/learning/controller/HomeController.java
+++ src/main/java/com/endava/learning/controller/HomeController.java
@@ -1,20 +1,13 @@
 package com.endava.learning.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.endava.learning.dao.SimpleDao;
-
 @Controller
 public class HomeController {
-
-	@Autowired
-	private SimpleDao simpleDao;
 	
 	@RequestMapping("/")
 	public String home(){
-		simpleDao.bla();
 		return "home";
 	}
 }
