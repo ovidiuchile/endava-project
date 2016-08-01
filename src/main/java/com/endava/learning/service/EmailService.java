d0aea3e82639222b0ba80bb597701199fa56a33e
Andrei Iulian Juravle <andrei.juravle@info.uaic.ro>
2016-08-01 10:50:34 +0300

    Mail Service

--- src/main/java/com/endava/learning/service/EmailService.java
+++ src/main/java/com/endava/learning/service/EmailService.java
@@ -3,6 +3,7 @@ package com.endava.learning.service;
 import java.util.Properties;
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -13,34 +14,37 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-	
-	public void send(String to, String subject, String email_body) {
-		String username = "woha.project";
-		String password = "proiecttw";
+
+	public void send(String to, String subject, String emailBody) {
+		final String username = "woha.project";
+		final String password = "proiecttw";
 
 		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "587");
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
 
-		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-			@Override
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
 		});
+
 		try {
+
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(username));
+			message.setFrom(new InternetAddress("woha.project@gmail.com"));
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
 			message.setSubject(subject);
-			message.setText(email_body);
+			message.setText(emailBody);
+
 			Transport.send(message);
-		} catch (Exception e) {
-			System.out.println(e);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
