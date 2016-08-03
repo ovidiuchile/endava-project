4829f42e4fc6cd661a2b551054304881ea3df523
Madalina Neghina <Madalina.Neghina@endava.com>
2016-08-03 15:37:49 +0300

    question controller added

--- src/main/java/com/endava/learning/controller/QuestionController.java
+++ src/main/java/com/endava/learning/controller/QuestionController.java
@@ -1,11 +1,28 @@
 package com.endava.learning.controller;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class QuestionController {
+	
+	/*@RequestMapping(value = "technologies/{technology_id}/topics/{topic_id}/test")
+	public ModelAndView getTest(){
+		ModelAndView model = new ModelAndView();
+		model.setViewName("test");
+		return model;
+	}*/
+	
+	@RequestMapping(value = "technologies/{technology_id}/topics/{topic_id}/test", method = RequestMethod.GET)
+	public HttpEntity<Resources<Resource<Question>>> getTest(){
+		
+	}
 
 }
---
+++ src/main/java/com/endava/learning/service/QuestionService.java
@@ -0,0 +1,5 @@
+package com.endava.learning.service;
+
+public class QuestionService {
+
+}
