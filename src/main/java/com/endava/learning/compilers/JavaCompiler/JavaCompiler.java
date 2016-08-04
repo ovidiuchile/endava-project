7b84da0894fdce8f1d6ee55eb26ba662582c5408
Andrei Iulian Juravle <andrei.juravle@info.uaic.ro>
2016-08-04 16:16:27 +0300

    add JavaCompiler

--- src/main/java/com/endava/learning/compilers/JavaCompiler/JavaCompiler.java
+++ src/main/java/com/endava/learning/compilers/JavaCompiler/JavaCompiler.java
@@ -1,32 +1,74 @@
 package com.endava.learning.compilers.JavaCompiler;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class JavaCompiler {
-	private final static MemoryJavaCompiler compiler = new MemoryJavaCompiler();
 
-	public static String compile(final String source){
-		System.setProperty("java.home", "C:\\Program Files\\Java\\jdk1.8.0_101");
-		String finalResult = new String();
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos);
-		PrintStream old = System.out;
-		System.setOut(ps);
-		System.setErr(ps);
-		try {
-			final Method greeting = compiler.compileStaticMethod("main", "Main", source);
-			greeting.invoke(null);
-		} catch(NullPointerException e){
-		}catch(ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e){
-		}finally {
-			System.out.flush();
-			finalResult = baos.toString();
-			System.setOut(old);
-			System.setErr(old);
+	private static String getLines(InputStream ins) throws Exception {
+		String result = "";
+		String line = null;
+		BufferedReader in = new BufferedReader(new InputStreamReader(ins));
+		while ((line = in.readLine()) != null) {
+			result += line + "\n";
 		}
-		return finalResult;
+		return result;
+	}
+
+	private static String runProcess(String command) throws Exception {
+		String result = "";
+		Process pro = Runtime.getRuntime().exec(command);
+		String stream = getLines(pro.getInputStream());
+		if (stream != null)
+			result += stream;
+		stream = getLines(pro.getErrorStream());
+		if (stream != null)
+			result += stream;
+		pro.waitFor();
+		return result;
+	}
+
+	private static void deleteFiles() {
+		try {
+			File folder = new File(".");
+			for (final File fileEntry : folder.listFiles()) {
+				String tempFile = fileEntry.getName();
+				// Delete if tempFile exists
+				File fileTemp = new File(tempFile);
+				if (fileTemp.exists() && (fileTemp.getName().contains(
+						".class") || fileTemp.getName().contains(".java") )) {
+					fileTemp.delete();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String compile(String source) {
+		String result="";
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("MyClass.java", "UTF-8");
+			writer.println(source);
+			writer.close();
+		} catch (UnsupportedEncodingException | FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			result+=runProcess("javac MyClass.java");
+			result+=runProcess("java Main");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		deleteFiles();
+		return result;
 	}
 }
--- src/main/java/com/endava/learning/controller/TestController.java
+++ src/main/java/com/endava/learning/controller/TestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.endava.learning.model.Answer;
-import com.endava.learning.model.Question;
 import com.endava.learning.service.TestService;
 
 @RestController
