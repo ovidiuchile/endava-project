50e4a9263591d569eea86c5ecea47d623336c103
Andrei Iulian Juravle <andrei.juravle@info.uaic.ro>
2016-08-05 11:02:07 +0300

    javaCompiler v5

--- src/main/java/com/endava/learning/compilers/JavaCompiler/JavaCompiler.java
+++ src/main/java/com/endava/learning/compilers/JavaCompiler/JavaCompiler.java
@@ -3,10 +3,13 @@ package com.endava.learning.compilers.JavaCompiler;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.lang.RandomStringUtils;
 
 public class JavaCompiler {
 
@@ -35,13 +38,14 @@ public class JavaCompiler {
 
 	private static void deleteFiles() {
 		try {
-			File folder = new File(".");
+			File folder = new File(System.getProperty("catalina.home") + "/compilerDirectory");
+			System.out.println(folder.getAbsolutePath());
 			for (final File fileEntry : folder.listFiles()) {
-				String tempFile = fileEntry.getName();
+				String tempFile = fileEntry.getAbsolutePath();
 				// Delete if tempFile exists
 				File fileTemp = new File(tempFile);
-				if (fileTemp.exists() && (fileTemp.getName().contains(
-						".class") || fileTemp.getName().contains(".java") )) {
+				if (fileTemp.exists()
+						&& (fileTemp.getName().contains(".class") || fileTemp.getName().contains(".java"))) {
 					fileTemp.delete();
 				}
 			}
@@ -51,20 +55,28 @@ public class JavaCompiler {
 	}
 
 	public static String compile(String source) {
-		String result="";
+		String result = "";
 		PrintWriter writer;
+		String javaFileName = RandomStringUtils.randomAlphanumeric(8)+".java";
 		try {
-			writer = new PrintWriter("MyClass.java", "UTF-8");
+			File javaFile = new File(System.getProperty("catalina.home") + "/compilerDirectory/" + javaFileName);
+			if (!javaFile.exists()) {
+				try {
+					javaFile.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			writer = new PrintWriter(javaFile, "UTF-8");
 			writer.println(source);
 			writer.close();
 		} catch (UnsupportedEncodingException | FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
-		
-		
+
 		try {
-			result+=runProcess("javac MyClass.java");
-			result+=runProcess("java Main");
+			result += runProcess("javac "+System.getProperty("catalina.home") + "/compilerDirectory/" + javaFileName);
+			result += runProcess("java -cp "+System.getProperty("catalina.home") + "/compilerDirectory Main");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
